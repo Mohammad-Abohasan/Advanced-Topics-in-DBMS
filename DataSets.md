85c94c124566de66562686cf81e9a5f4c3896eae
Mohammad-Abohasan <m.s.abuhasan@students.ptuk.edu.ps>
2023-05-22 23:43:32 +0300

    Full Code and Testing done

---
+++ DataSets.md
@@ -0,0 +1,41 @@
+##Input
+**
+**
+###Movies
+1, CS, Comedy
+2, ADBMS, Action
+3, DSP, Children
+4, OS, Comedy
+5, WEB, Comedy
+6, AI, Children
+7, SIGNALS, Action
+
+**
+###Users
+1, Male, 17
+2, Female, 28
+3, Female, 25
+4, Male, 27
+5, Male, 35
+6, Female, 30
+
+**
+###Ratings
+2, 4, 3
+4, 3, 3
+4, 4, 4
+1, 3, 1
+5, 4, 4
+5, 7, 5
+5, 1, 1
+
+**
+**
+##Output
+**
+**
+1	CS, 0.0
+3	DSP, 3.0
+4	OS, 3.6666666666666665
+5	WEB, 0.0
+6	AI, 0.0
--- FirstJob.java
+++ FirstJob.java
@@ -1,5 +1,5 @@
 import java.io.IOException;
-
+import java.util.ArrayList;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
@@ -13,77 +13,87 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class FirstJob {
 
-    public static class UserMapper extends Mapper<LongWritable, Text, Text, Text> {
-        private Text userID = new Text(), result = new Text();
+  public static class UserMapper
+    extends Mapper<LongWritable, Text, Text, Text> {
 
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            String v = value.toString();
-            String[] tokens = v.split(", ");
-            if (Integer.parseInt(tokens[2]) > 25) {
-                userID.set(tokens[0]);
-                result.set("User");
-                context.write(userID, result);
-            }
+    private Text userID = new Text(), result = new Text();
+
+    public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException {
+      String v = value.toString();
+      String[] tokens = v.split(", ");
+      if (Integer.parseInt(tokens[2]) > 25) {
+        userID.set(tokens[0]);
+        result.set("User");
+        context.write(userID, result);
+      }
+    }
+  }
+
+  public static class RatingMapper
+    extends Mapper<LongWritable, Text, Text, Text> {
+
+    private Text userID = new Text(), result = new Text();
+
+    public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException {
+      String v = value.toString();
+      String[] tokens = v.split(", ");
+      if (Double.parseDouble(tokens[2]) > 2) {
+        userID.set(tokens[0]);
+        result.set(tokens[1] + ", " + tokens[2]);
+        context.write(userID, result);
+      }
+    }
+  }
+
+  public static class UserRatingReducer
+    extends Reducer<Text, Text, Text, Text> {
+
+    private Text myKey = new Text("MR");
+
+    public void reduce(Text key, Iterable<Text> values, Context context)
+      throws IOException, InterruptedException {
+      ArrayList<Text> ratingG2 = new ArrayList<Text>();
+      int valuesSize = 0, ratsSize = 0;
+      for (Text val : values) {
+        String v = val.toString();
+        if (v.charAt(0) != 'U') {
+          ratingG2.add(new Text(v));
+          ratsSize++;
         }
-    }
-
-    public static class RatingMapper extends Mapper<LongWritable, Text, Text, Text> {
-        private Text userID = new Text(), result = new Text();
-
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            String v = value.toString();
-            String[] tokens = v.split(", ");
-            if (Double.parseDouble(tokens[2]) > 2) {
-                userID.set(tokens[0]);
-                result.set("Rating, " + tokens[1] + ", " + tokens[2]);
-                context.write(userID, result);
-            }
+        valuesSize++;
+      }
+      if (valuesSize != ratsSize) {
+        for (Text val : ratingG2) {
+          context.write(myKey, val);
         }
+      }
     }
+  }
 
-    public static class UserRatingReducer extends Reducer<Text, Text, Text, Text> {
-        private Text myKey = new Text(), result = new Text();
-
-        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-            boolean ageG25 = false, ratingG2 = false;
-            for (Text val : values) {
-                String v = val.toString();
-                String[] tokens = v.split(", ");
-                if (tokens[0].equals("User")) {
-                    ageG25 = true;
-                }
-                if (tokens[0].equals("Rating")) {
-                    ratingG2 = true;
-                }
-                if (ageG25 && ratingG2) {
-                    break;
-                }
-            }
-            if (ageG25 && ratingG2) {
-                for (Text val : values) {
-                    String v = val.toString();
-                    String[] tokens = v.split(", ");
-                    if (tokens[0].equals("Rating")) {
-                        myKey.set("MR");
-                        result.set(tokens[0] + ", " + tokens[1]);
-                        context.write(myKey, result);
-                    }
-                }
-            }
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "first job");
-        job.setJarByClass(FirstJob.class);
-        job.setCombinerClass(UserRatingReducer.class);
-        job.setReducerClass(UserRatingReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, UserMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, RatingMapper.class);
-        FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
-    }
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "first job");
+    job.setJarByClass(FirstJob.class);
+    //job.setNumReduceTasks(0);
+    //job.setCombinerClass(UserRatingReducer.class);
+    job.setReducerClass(UserRatingReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+    MultipleInputs.addInputPath(
+      job,
+      new Path(args[0]),
+      TextInputFormat.class,
+      UserMapper.class
+    );
+    MultipleInputs.addInputPath(
+      job,
+      new Path(args[1]),
+      TextInputFormat.class,
+      RatingMapper.class
+    );
+    FileOutputFormat.setOutputPath(job, new Path(args[2]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
 }
